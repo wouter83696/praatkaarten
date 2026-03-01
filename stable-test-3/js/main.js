9d8c1513c7a1e2053c90147db6e4d6f83009a7c4
wouter83696 <w.gerretsen@gmail.com>
2026-03-01 09:20:34 +0100

    Add files via upload

--- stable-test-3/js/main.js
+++ stable-test-3/js/main.js
@@ -4,7 +4,7 @@
   var w = window;
   var doc = document;
   var PK = w.PK = w.PK || {};
-  var ASSET_V = '1.28';
+  var ASSET_V = '1.29';
   var page = (doc.body && doc.body.getAttribute) ? (doc.body.getAttribute('data-page') || '') : '';
   var pathName = '';
   var lastRuntimeError = '';
